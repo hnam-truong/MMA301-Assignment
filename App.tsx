87fe9e32367ef5f41833173c4231a23475364ed8
hnam-truong <truonghahaonam@gmail.com>
2024-09-25 11:21:53 +0700

    set up

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import "@/global.css";
+import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
+
+export default function App() {
+  return <GluestackUIProvider>Dung roi</GluestackUIProvider>;
+}
