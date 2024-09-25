f14fda278aaa1a7615c685020144639963cade84
hnam-truong <truonghahaonam@gmail.com>
2024-09-25 13:44:04 +0700

    set up

--- App.tsx
+++ App.tsx
@@ -2,9 +2,5 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 
 export default function App() {
-  return (
-    <GluestackUIProvider>
-
-    </GluestackUIProvider>
-  );
+  return <GluestackUIProvider></GluestackUIProvider>;
 }
